ef6a2e6c8e1fffdb055ad7ccf02413393c7a0ac8
Kate Zabolotskih <zabolotskihkate@gmail.com>
2020-10-06 02:04:21 +0300

    errors due to lack of sleep fixed

--- Lab1/BayesianClassification.py
+++ Lab1/BayesianClassification.py
@@ -3,6 +3,7 @@ from math import sqrt, log, exp, fabs, erf
 import numpy as np
 from matplotlib import pyplot as plt
 
+# functions for counting covariation matrix made of consistent estimates
 def s_k_l_j(m, l, j, n_k, means):
     s = 0
     for i in range(n_k):
@@ -25,7 +26,15 @@ def covariation_matrix(s1, s2, mean1, mean2):
     cov = ((n_1 - 1) * s1 + (n_2 - 1) * s2) / (n_1 + n_2 - 2)
     return cov
 
+# the main class of the task of the discriminant analysis
 class BayesianClassification:
+    # initialization of consistent estimates:
+    # mean, covariation matrix, z_1, z_2, q_1, q_2:
+    # q_1 - the number of elements from the first training sample
+    # that were mistakenly classified to the second one
+    # q_2 - the number of elements from the second training sample
+    # that were mistakenly classified to the first one
+    # also counting mahalanobis distance and the probability of erroneous classification
     def __init__(self, sample1, sample2):
         self.sample1 = sample1
         self.sample2 = sample2
@@ -55,7 +64,7 @@ class BayesianClassification:
                 self.q1 += 1
         for i in range(self.n2):
             x = sample2[:, i]
-            if self.classify(x, is_q_set=False) == 0:
+            if self.classify(x, is_q_set=False) == 1:
                 self.q2 += 1
         self.D = 0
         self.DH = 0
@@ -74,7 +83,7 @@ class BayesianClassification:
             return 1
 
     def plot_data(self, sample_1, sample_2):
-        f = lambda x, y: ((- self.a[0] * x - self.a[1] * y) + np.full(x.shape, self.c + log(self.q2/self.q1))) / self.a[2]
+        q1, q2 = 0, 0
         x_max = max(max(sample_1[0]), max(sample_2[0])) + 1
         x_min = min(min(sample_1[0]), min(sample_2[0])) - 1
         y_max = max(max(sample_1[1]), max(sample_2[1])) + 1
@@ -85,45 +94,57 @@ class BayesianClassification:
         x = np.linspace(x_min, x_max, 10)
         y = np.linspace(y_min, y_max, 10)
         X, Y = np.meshgrid(x, y)
-        Z = f(X, Y)
 
         fig = plt.figure()
         ax = fig.add_subplot(projection='3d')
+
         for i in range(n_1):
             x = sample_1[:, i]
             if self.classify(x) == 1:
-                ax.scatter(x[0], x[1], x[2], c='teal', marker='+')
-            else:
                 ax.scatter(x[0], x[1], x[2], c='teal', marker='o')
+            else:
+                ax.scatter(x[0], x[1], x[2], c='teal', marker='+')
+                q1 += 1
+
         for i in range(n_2):
             x = sample_2[:, i]
-            if self.classify(x) == 1:
-                ax.scatter(x[0], x[1], x[2], c='mediumpurple', marker='*')
-            else:
+            if self.classify(x) == 0:
                 ax.scatter(x[0], x[1], x[2], c='mediumpurple', marker='^')
+            else:
+                ax.scatter(x[0], x[1], x[2], c='mediumpurple', marker='*')
+                q2 += 1
+
+        f = lambda x, y: ((- self.a[0] * x - self.a[1] * y) + np.full(x.shape, self.c + log(self.q2 / self.q1))) / self.a[2]
+        Z = f(X, Y)
+
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
         ax.set_zlabel('Z')
         ax.plot_wireframe(X, Y, Z, color='grey')
         plt.show()
+        print('q1 = ' + str(q1) + ' q2 = ' + str(q2))
         return
 
     def culc_prob(self):
-        F = lambda x: 1/2*(1 + erf(x / sqrt(2)))
+        F = lambda x: 1 / 2 * (1 + erf(x / sqrt(2)))
         K = log(self.q2 / self.q1)
-        self.P_12 = F((- K - self.D/2)/
-                      sqrt(self.D))
 
-        self.P_21 = F((K - self.D/2)/
-                      sqrt(self.D))
-    def culc_mahalanobis_distance(self, p = 3):
-        s = 0
-        for i in range(len(self.cov_matrix[0])):
-            for j in range(len(self.cov_matrix[0])):
-                s += self.cov_matrix[i][j] * self.a[i] * self.a[j]
+        self.P_12 = F((-K - self.DH / 2) / sqrt(self.D))
 
-        self.D = (self.z1 - self.z2) * (self.z1 - self.z2) / s
-        self.DH = (self.n1 + self.n2 - p - 3) * self.D / (self.n1 + self.n2 - 2) - p * (1/self.n1 + 1/self.n2)
+        self.P_21 = F((K - self.DH / 2) / sqrt(self.D))
+
+    def culc_mahalanobis_distance(self, p=3):
+        sample = np.concatenate((self.sample1, self.sample2), axis=1)
+        means = np.array([np.mean(row) for row in sample])
+        sum = 0
+        for i in range(p):
+            for j in range(p):
+                sum += self.a[i] * s_k_l_j(sample, i, j, self.n1 + self.n2, means) * self.a[j]
+
+        self.D = (self.z1 - self.z2) * (self.z1 - self.z2) / (sum * sum)
+        self.DH = (self.n1 + self.n2 - p - 3) / (self.n1 + self.n2 - 2) * self.D - p * (1 / self.n1 + 1 / self.n2)
+
+
 
 
 
--- Lab1/testing.py
+++ Lab1/testing.py
@@ -8,13 +8,15 @@ if not os.path.exists(data_path):
     exit(1)
 
 p = 3
-col1 = 3
-col2 = 5
+col1 = 2
+col2 = 3
 col3 = 8
 
 matrix = np.loadtxt(data_path)
 
-def get_sample(matrix, cols, n, marker, p, start_from = 0):
+ # выбираем из матрицы "объект-свойсво" столбцы (признаки) в количесве p штук)
+ # составляем обучающую или тестовую матрицу таким образом
+def get_sample_from_data(matrix, cols, n, marker, p, start_from = 0):
     sample = np.zeros((p, n))
     col1_i = 0
     offset = 0
@@ -30,19 +32,27 @@ def get_sample(matrix, cols, n, marker, p, start_from = 0):
             col1_i += 1
     return sample
 
-n1 = 100
-sample1 = get_sample(matrix, [col1, col2, col3], n1, marker=1, p=p)
-n2 = 100
-sample2 = get_sample(matrix, [col1, col2, col3], n2, marker=2, p=p)
+# составляем обучающие выборки
+n1 = 300
+sample1 = get_sample_from_data(matrix, [col1, col2, col3], n1, marker=1, p=p)
+n2 = 300
+sample2 = get_sample_from_data(matrix, [col1, col2, col3], n2, marker=2, p=p)
+# "обучаем" классификатор
 BC = BC.BayesianClassification(sample1, sample2)
+print('training samples:')
 BC.plot_data(sample1, sample2)
 
-n3 = 10
-sample3 = get_sample(matrix, [col1, col2, col3], n3, marker=1, p=p, start_from=20)
-n4 = 10
-sample4 = get_sample(matrix, [col1, col2, col3], n4, marker=2, p=p, start_from=20)
+# составляем тестовые выборки
+n3 = 50
+sample3 = get_sample_from_data(matrix, [col1, col2, col3], n3, marker=1, p=p, start_from=20)
+n4 = 50
+sample4 = get_sample_from_data(matrix, [col1, col2, col3], n4, marker=2, p=p, start_from=20)
+# используем уже "обученный" классификатор для разделения данных тестовой выборки
+print('testing samples:')
 BC.plot_data(sample3, sample4)
 
+# дынные обученного классификатора
+print('characteristics of classifier:')
 print('P_21 = ' + str(BC.P_21) + '\n'
       'P_12 = ' + str(BC.P_12) + '\n'
       'q1 = ' + str(BC.q1) + '\n'
