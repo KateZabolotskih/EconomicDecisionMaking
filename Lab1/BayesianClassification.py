afcfd8b7996979e000b5b385dc4b0dceff2bbf71
Kate Zabolotskih <zabolotskihkate@gmail.com>
2020-10-16 18:15:15 +0300

    A little beauty and logic. But there is a bug in calculating the probability. But I'm very lazy

--- Lab1/BayesianClassification.py
+++ Lab1/BayesianClassification.py
@@ -29,11 +29,7 @@ def covariation_matrix(s1, s2, mean1, mean2):
 # the main class of the task of the discriminant analysis
 class BayesianClassification:
     # initialization of consistent estimates:
-    # mean, covariation matrix, z_1, z_2, q_1, q_2:
-    # q_1 - the number of elements from the first training sample
-    # that were mistakenly classified to the second one
-    # q_2 - the number of elements from the second training sample
-    # that were mistakenly classified to the first one
+    # mean, covariation matrix, z1, z2, q1, q2
     # also counting mahalanobis distance and the probability of erroneous classification
     def __init__(self, sample1, sample2):
         self.sample1 = sample1
@@ -41,34 +37,34 @@ class BayesianClassification:
         self.n1 = len(sample1[0])
         self.n2 = len(sample2[0])
 
-        self.mean1 = np.array([np.mean(row) for row in sample1])
-        self.mean2 = np.array([np.mean(row) for row in sample2])
+        self.mean1 = np.array([np.mean(row) for row in self.sample1])
+        self.mean2 = np.array([np.mean(row) for row in self.sample2])
 
-        self.cov_matrix = covariation_matrix(sample1, sample2, self.mean1, self.mean2)
+        self.cov_matrix = covariation_matrix(self.sample1, self.sample2, self.mean1, self.mean2)
         self.a = np.linalg.inv(self.cov_matrix).dot(self.mean1 - self.mean2)
 
         self.z1, self.z2 = 0, 0
         for i in range(self.n1):
-            self.z1 += self.a.dot(sample1[:, i])
+            self.z1 += self.a.dot(self.sample1[:, i])
         for i in range(self.n2):
-            self.z2 += self.a.dot(sample2[:, i])
+            self.z2 += self.a.dot(self.sample2[:, i])
         self.z1 /= self.n1
         self.z2 /= self.n2
 
-        self.q1 = self.n1 / (self.n1 + self.n2)
-        self.q2 = self.n2 / (self.n1 + self.n2)
+        self.r1 = self.n1 / (self.n1 + self.n2)
+        self.r2 = self.n2 / (self.n1 + self.n2)
 
-        self.c = (self.z1 + self.z2) / 2 + log(self.q1 / self.q2)
+        self.c = (self.z1 + self.z2) / 2 + log(self.r1 / self.r2)
 
         self.D = 0
         self.DH = 0
-        self.culc_mahalanobis_distance()
+        self._culc_mahalanobis_distance()
         self.P_12 = 0
         self.P_21 = 0
-        self.culc_prob()
+        self._culc_prob()
 
-    def classify(self, array, is_q_set=True):
-        compare = lambda arr: arr.dot(self.a) < self.c + log(self.q2 / self.q1)
+    def _classify(self, array, is_q_set=True):
+        compare = lambda arr: arr.dot(self.a) < self.c + log(self.r2 / self.r1)
         if not is_q_set:
             compare = lambda arr: arr.dot(self.a) < self.c
         if compare(array):
@@ -76,8 +72,7 @@ class BayesianClassification:
         else:
             return 1
 
-    def plot_data(self, sample_1, sample_2):
-        q1, q2 = 0, 0
+    def plot_data_3D(self, sample_1, sample_2):
         x_max = max(max(sample_1[0]), max(sample_2[0])) + 1
         x_min = min(min(sample_1[0]), min(sample_2[0])) - 1
         y_max = max(max(sample_1[1]), max(sample_2[1])) + 1
@@ -96,38 +91,51 @@ class BayesianClassification:
 
         for i in range(n_1):
             x = sample_1[:, i]
-            if self.classify(x) == 1:
+            if self._classify(x) == 1:
                 ax.scatter(x[0], x[1], x[2], c='teal', marker='o')
             else:
                 ax.scatter(x[0], x[1], x[2], c='teal', marker='+')
-                q1 += 1
 
         for i in range(n_2):
             x = sample_2[:, i]
-            if self.classify(x) == 0:
+            if self._classify(x) == 0:
                 ax.scatter(x[0], x[1], x[2], c='mediumpurple', marker='^')
             else:
                 ax.scatter(x[0], x[1], x[2], c='mediumpurple', marker='*')
-                q2 += 1
-
 
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
         ax.set_zlabel('Z')
         ax.plot_wireframe(X, Y, Z, color='grey')
         plt.show()
-        print('q1 = ' + str(q1) + ' q2 = ' + str(q2))
         return
 
-    def culc_prob(self):
+    def classify(self, sample_1, sample_2):
+        q1 = 0
+        q2 = 0
+        n1 = len(sample_1[0])
+        n2 = len(sample_2[0])
+
+        for i in range(n1):
+            x = sample_1[:, i]
+            if self._classify(x) == 1:
+                q1 += 1
+
+        for i in range(n2):
+            x = sample_2[:, i]
+            if self._classify(x) == 0:
+                q2 += 1
+        print('q1 = ' + str(q1) + ' q2 = ' + str(q2))
+
+    def _culc_prob(self):
         F = lambda x: 1 / 2 * (1 + erf(x / sqrt(2)))
-        K = log(self.q2 / self.q1)
+        K = log(self.r2 / self.r1)
 
         self.P_12 = F((-K - self.DH / 2) / sqrt(self.D))
 
         self.P_21 = F((K - self.DH / 2) / sqrt(self.D))
 
-    def culc_mahalanobis_distance(self, p=3):
+    def _culc_mahalanobis_distance(self, p=3):
         _sum = (self.a.dot(self.cov_matrix)).dot(self.a)
 
         self.D = (self.z1 - self.z2) * (self.z1 - self.z2) / (_sum * _sum)
