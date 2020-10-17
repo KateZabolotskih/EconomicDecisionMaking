983f7a54f68c25898d5a9198ced11e4409f02832
Kate Zabolotskih <zabolotskihkate@gmail.com>
2020-10-17 04:47:56 +0300

    new report

--- Lab1/testing.py
+++ Lab1/testing.py
@@ -7,12 +7,12 @@ if not os.path.exists(data_path):
     print(f"cannot find data file: {data_path}")
     exit(1)
 
-cov = [[9, 6, 2],
-       [6, 11, 4],
-       [2, 4, 20]]
+cov = [[7, 3,  2],
+       [3, 10, 5],
+       [2, 5,  8]]
 
-mean1 = (2, 2, 2)
-mean2 = (-2, 2, 2)
+mean1 = (5, 5, 5)
+mean2 = (-5, 5, 5)
 X = np.random.multivariate_normal(mean1, cov, 110).transpose()
 Y = np.random.multivariate_normal(mean2, cov, 90).transpose()
 
@@ -23,6 +23,8 @@ W = np.random.multivariate_normal(mean2, cov, 500).transpose()
 
 bc.classify(X, Y)
 bc.classify(P, W)
+#bc.plot_data_3D(X, Y)
+#bc.plot_data_3D(P, W)
 
 print('P_21 = ' + str(bc.P_21) + '\n'
       'P_12 = ' + str(bc.P_12) + '\n'
@@ -32,12 +34,18 @@ print('P_21 = ' + str(bc.P_21) + '\n'
       'DH = ' + str(bc.DH) + '\n')
 
 
-p = 3
-col1 = 0
-col2 = 2
+#p = 5
+#col1 = 0
+#col2 = 2
+#col3 = 9
+#col4 = 11
+#col5 = 20
+
+p = 4
+col1 = 1
+col2 = 3
 col3 = 9
-col4 = 11
-col5 = 20
+col4 = 19
 
 matrix = np.loadtxt(data_path)
 
@@ -61,19 +69,19 @@ def get_sample_from_data(matrix, cols, n, marker, p, start_from = 0):
 
 # составляем обучающие выборки
 n1 = 100
-sample1 = get_sample_from_data(matrix, [col1, col2, col3, col4, col5], n1, marker=1, p=p)
-n2 = 110
-sample2 = get_sample_from_data(matrix, [col1, col2, col3, col4, col5], n2, marker=2, p=p)
+sample1 = get_sample_from_data(matrix, [col1, col2, col3, col4], n1, marker=2, p=p)
+n2 = 90
+sample2 = get_sample_from_data(matrix, [col1, col2, col3, col4], n2, marker=1, p=p)
 # "обучаем" классификатор
 BC = BC.BayesianClassification(sample1, sample2)
 print('training samples:')
 BC.classify(sample1, sample2)
 
 # составляем тестовые выборки
-n3 = 300
-sample3 = get_sample_from_data(matrix, [col1, col2, col3, col4, col5], n3, marker=1, p=p, start_from=20)
-n4 = 400
-sample4 = get_sample_from_data(matrix, [col1, col2, col3, col4, col5], n4, marker=2, p=p, start_from=20)
+n3 = 400
+sample3 = get_sample_from_data(matrix, [col1, col2, col3, col4], n3, marker=2, p=p, start_from=20)
+n4 = 300
+sample4 = get_sample_from_data(matrix, [col1, col2, col3, col4], n4, marker=1, p=p, start_from=20)
 # используем уже "обученный" классификатор для разделения данных тестовой выборки
 print('testing samples:')
 BC.classify(sample3, sample4)
@@ -82,8 +90,8 @@ BC.classify(sample3, sample4)
 print('characteristics of classifier:')
 print('P_21 = ' + str(BC.P_21) + '\n'
       'P_12 = ' + str(BC.P_12) + '\n'
-      'q1 = ' + str(BC.r1) + '\n'
-      'q2 = ' + str(BC.r2) + '\n'
+      'r1 = ' + str(BC.r1) + '\n'
+      'r2 = ' + str(BC.r2) + '\n'
       'D = ' + str(BC.D) + '\n'
       'DH = ' + str(BC.DH) + '\n')
 
